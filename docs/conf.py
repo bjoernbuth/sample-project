33106c71966f2ecd764f616f7a6a2c99bb8b6015
bjoernbuth <bjoernbuth76@gmail.com>
2024-05-15 16:56:36 +0200

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,12 +21,12 @@ from sphinx_gallery.sorting import FileNameSortKey
 
 # -- Project information -----------------------------------------------------
 
-project = 'sample-project'
-copyright = '2020, <Author>'
-author = '<Author>'
+project = "sphinx_sample"
+copyright = "2024, Bjoern"
+author = "Bjoern"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -35,19 +35,19 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx_gallery.gen_gallery',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    ]
+    "sphinx.ext.autodoc",
+    "sphinx_gallery.gen_gallery",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -55,31 +55,31 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # sphinx-gallery configuration
 sphinx_gallery_conf = {
     # path to your example scripts
-    'examples_dirs': ['../sample-gallery-1', '../sample-gallery-2'],
+    "examples_dirs": ["../sample-gallery-1", "../sample-gallery-2"],
     # path to where to save gallery generated output
-    'gallery_dirs': ['auto_gallery-1', 'auto_gallery-2'],
+    "gallery_dirs": ["auto_gallery-1", "auto_gallery-2"],
     # specify that examples should be ordered according to filename
-    'within_subsection_order': FileNameSortKey,
+    "within_subsection_order": FileNameSortKey,
     # directory where function granular galleries are stored
-    'backreferences_dir': 'gen_modules/backreferences',
+    "backreferences_dir": "gen_modules/backreferences",
     # Modules for which function level galleries are created.  In
     # this case sphinx_gallery and numpy in a tuple of strings.
-    'doc_module': ('SampleModule'),
+    "doc_module": ("SampleModule"),
 }
 
 # configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/{.major}'.format(sys.version_info), None),
-    'matplotlib': ('https://matplotlib.org/', None),
-    'pandas': ('https://pandas.pydata.org/', None),
+    "python": ("https://docs.python.org/{.major}".format(sys.version_info), None),
+    "matplotlib": ("https://matplotlib.org/", None),
+    "pandas": ("https://pandas.pydata.org/", None),
 }
